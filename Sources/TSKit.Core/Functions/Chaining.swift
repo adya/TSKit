009ebe9b0814041c448e15af191225d95a4bfd4f
Arkadii Hlushchevskyi <adya.glu@gmail.com>
2022-02-18 13:25:20 +0200

    TSKit.Core: Polished currying. Added documentation and more arguments variations.

--- Sources/TSKit.Core/Functions/Chaining.swift
+++ Sources/TSKit.Core/Functions/Chaining.swift
@@ -18,9 +18,8 @@ infix operator ==> : ChainingPrecedence
 infix operator ==>? : ChainingPrecedence
 
 public func ==><A, B>(_ lhs: A, _ rhs: (A) throws -> B) rethrows -> B {
-    return try rhs(lhs)
+    try rhs(lhs)
 }
-
 
 @discardableResult
 public func ==><A>(_ lhs: A, _ rhs: (A) throws -> Void) rethrows -> A {
@@ -36,27 +35,27 @@ public func ==><A>(_ lhs:  () throws -> A, _ rhs: (A) throws -> Void) rethrows -
 }
 
 public func ==><A, B>(_ lhs: () throws -> A, _ rhs: (A) throws -> B) rethrows -> B {
-    return try rhs(try lhs())
+    try rhs(try lhs())
 }
 
 public func ==>?<A, B>(_ lhs: A?, _ rhs: (A) throws -> B?) rethrows -> B? {
-    return try lhs.flatMap(rhs)
+    try lhs.flatMap(rhs)
 }
 
 public func ==>?<A, B>(_ lhs: () throws -> A?, _ rhs: (A) throws -> B?) rethrows -> B? {
-    return try lhs().flatMap(rhs)
+    try lhs().flatMap(rhs)
 }
 
 public func =><A, B, C>(_ lhs: @escaping (A) -> B, _ rhs: @escaping (B) -> C) -> (A) -> C {
-    return { rhs(lhs($0)) }
+    { rhs(lhs($0)) }
 }
 
 public func =><A, B, C>(_ lhs: @escaping (A) -> B?, _ rhs: @escaping (B) -> C?) -> (A) -> C? {
-    return { lhs($0).flatMap(rhs) }
+    { lhs($0).flatMap(rhs) }
 }
 
 public func =><A, B>(_ lhs: @escaping (A) -> B, _ rhs: @escaping (B) -> Void) -> (A) -> B {
-    return {
+    {
         let b = lhs($0)
         rhs(b)
         return b
@@ -64,7 +63,7 @@ public func =><A, B>(_ lhs: @escaping (A) -> B, _ rhs: @escaping (B) -> Void) ->
 }
 
 public func =><A>(_ lhs: @escaping () -> A, _ rhs: @escaping (A) -> Void) -> () -> A {
-    return {
+    {
         let a = lhs()
         rhs(a)
         return a
@@ -72,7 +71,7 @@ public func =><A>(_ lhs: @escaping () -> A, _ rhs: @escaping (A) -> Void) -> () 
 }
 
 public func =>?<A, B>(_ lhs: @escaping (A) -> B?, _ rhs: @escaping (B) -> Void) -> (A) -> B? {
-    return {
+    {
         let b = lhs($0)
         b.flatMap(rhs)
         return b
@@ -80,18 +79,9 @@ public func =>?<A, B>(_ lhs: @escaping (A) -> B?, _ rhs: @escaping (B) -> Void) 
 }
 
 public func =>?<A>(_ lhs: @escaping () -> A?, _ rhs: @escaping (A) -> Void) -> () -> A? {
-    return {
+    {
         let a = lhs()
         a.flatMap(rhs)
         return a
     }
-}
-
-public func curry<A, B, C>(f: @escaping (A, B) -> C) -> (A) -> (B) -> C {
-    return { a in { b in f(a, b) } }
-}
-
-
-public func curry<A, B, C, D>(f: @escaping (A, B, C) -> D) -> (A) -> (B) -> (C) -> D {
-    return { a in { b in { c in f(a, b, c) } } }
 }
