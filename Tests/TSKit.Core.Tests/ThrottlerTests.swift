f5ccfd1fff6bbc0ddb4083284af7c9f3714776f1
Arkadii Hlushchevskyi <adya.glu@gmail.com>
2022-02-28 11:39:42 +0200

    TSKit.Core: Added Debouncer and Throttler.

---
+++ Tests/TSKit.Core.Tests/ThrottlerTests.swift
@@ -0,0 +1,80 @@
+import XCTest
+@testable import TSKit_Core
+
+class ThrottlerTests: XCTestCase {
+    
+    private var throttler: Throttler!
+    
+    private var queue: DispatchQueue!
+    
+    override func setUp() {
+        queue = .init(label: "TestingQueue")
+        throttler = .init(throttlingInterval: 0.45, deafultQueue: queue)
+        super.setUp()
+    }
+
+    func testThrottle() {
+        var values: [Int] = []
+            
+        let q = DispatchQueue(label: "Scheduler")
+        let executeInSeconds = 1
+        let iterations = 10
+//        let expectedValues = [1, 50, 51, 100] // For 100 iterations
+        let expectedValues = [1, 5, 6, 10] // For 10 iterations
+        let delay = (executeInSeconds * 1000)/iterations
+        for i in (1...iterations) {
+            q.asyncAfter(deadline: .now() + .milliseconds(i * delay)) { [self] in
+                throttler.throttle {
+                    values.append(i)
+                }
+            }
+        }
+        q.asyncAfter(deadline: .now() + .milliseconds((executeInSeconds*1000) + 200)) {
+            XCTAssertEqual(values, expectedValues)
+        }
+        
+        wait(TimeInterval(executeInSeconds)*1.5)
+    }
+    
+    func testThrottleFirst() {
+        var values: [Int] = []
+            
+        let q = DispatchQueue(label: "Scheduler")
+        let executeInSeconds = 1
+        let iterations = 10
+//        let expectedValues = [1, 2, 51, 52] // For 100 iterations
+        let expectedValues = [1, 2, 6, 7] // For 10 iterations
+        let delay = (executeInSeconds * 1000)/iterations
+        var counter = 1
+        for i in (1...iterations) {
+            q.asyncAfter(deadline: .now() + .milliseconds(i * delay)) { [self] in
+                print("\(counter * delay)ms: Sending #\(i)")
+                throttler.throttleFirst {
+                    print("\(counter * delay)ms: Using #\(i)")
+                    values.append(i)
+                }
+                counter += 1
+            }
+        }
+        q.asyncAfter(deadline: .now() + .milliseconds((executeInSeconds*1000) + 500)) {
+            XCTAssertEqual(values, expectedValues)
+        }
+        
+        wait(TimeInterval(executeInSeconds)*2)
+    }
+}
+
+private extension ThrottlerTests {
+    
+    /// Waits for a stub expectation that allows throttling test to wait until all throttling activity finishes.
+    ///
+    /// This method is used instead of expectations within throttling because throttling may vary at number of calls to the passed block, which will require similar code to be written for every test.
+    func wait(_ time: TimeInterval = 2) {
+        let expectation = self.expectation(description: "Waiting for throttling to end")
+        DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(Int(time * 1000))) {
+            expectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: time + 0.1, handler: nil)
+    }
+}
